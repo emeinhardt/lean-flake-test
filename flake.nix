77f143166f6dbebe3e865ec4c604fe57230d21b4
Eric Meinhardt <ericmeinhardt@gmail.com>
2022-10-02 16:14:16 -0700

    add dev shell (with elan)

--- flake.nix
+++ flake.nix
@@ -1,11 +1,14 @@
 {
   description = "Demand Analysis";
 
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   inputs.lean.url = github:leanprover/lean4;
   inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.flake-utils.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, lean, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, lean}: flake-utils.lib.eachDefaultSystem (system:
     let
+      pkgs = nixpkgs.legacyPackages.${system};
       leanPkgs = lean.packages.${system};
       pkg = leanPkgs.buildLeanPackage {
         name = "DmdAnal";  # must match the name of the top-level .lean file
@@ -17,5 +20,9 @@
       };
 
       defaultPackage = pkg.modRoot;
+
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [ elan ];
+      };
     });
 }
