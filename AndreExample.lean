bccf0a7b5d4061b397873e0bb520d0ff5fe694bf
Eric Meinhardt <ericmeinhardt@gmail.com>
2022-10-02 17:07:51 -0700

    Add link to zulip thread

--- AndreExample.lean
+++ AndreExample.lean
@@ -2,3 +2,5 @@ import Lean
 
 #print Lean.Elab.Command.Scope
 #print Bool
+
+-- see https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/Go.20to.20definition.20not.20finding.20some.20definitions/near/300694309
